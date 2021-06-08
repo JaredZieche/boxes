f44145e854bc9b75ed1a4ddbf8b6326c5f435f7b
jaredzieche <jared.zieche@va.gov>
2021-06-08 09:07:45 -0400

    updated organization

---
+++ centos8/scripts/root_setup.sh
@@ -0,0 +1,16 @@
+#!javascript
+#!/bin/bash
+set -e
+# Updating and Upgrading dependencies
+sudo yum update -y -qq > /dev/null
+sudo yum upgrade -y -qq > /dev/null
+# Install necessary libraries for guest additions and Vagrant NFS Share
+sudo yum -y -q install linux-headers-$(uname -r) build-essential dkms nfs-common
+# Install necessary dependencies
+sudo yum -y -q install curl wget git tmux firefox xvfb vim
+# Setup sudo to allow no-password sudo for "admin"
+groupadd -r admin
+usermod -a -G admin vagrant
+cp /etc/sudoers /etc/sudoers.orig
+sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
+sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
