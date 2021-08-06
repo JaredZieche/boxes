8cdf03ee59aee3ce8d3c0dbedc87072b04664d36
Zieche <Jared.Zieche@va.gov>
2021-08-06 14:41:42 -0400

    updated setup.sh to troubleshoot rhel packages

--- RHEL8/setup.sh
+++ RHEL8/setup.sh
@@ -2,6 +2,11 @@
 # Install Python.
 # Add the EPEL repository, and install Ansible.
 dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+dnf config-manager --save --setopt=*.gpgcheck=0
+dnf config-manager --set-enabled epel
+dnf -y update
 dnf -y install python3-setuptools python3 python3-pip
 alternatives --set python /usr/bin/python3
 # Upgrade Pip.
